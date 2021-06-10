427fce240e94161258b6fb2d845e10b7f6d8287e
reetah589 <reetah589@gmail.com>
2021-06-10 13:33:17 -0500

    first commit testing push

---
+++ script.js
@@ -0,0 +1,15 @@
+//You’ll need to use the MySQL2 package 
+
+//(Links to an external site.) 
+
+//to connect to your MySQL database and perform queries, 
+
+//the Inquirer package 
+
+//(Links to an external site.) 
+
+//to interact with the user via the command-line, 
+
+//and the console.table package (Links to an external site.) 
+
+//to print MySQL rows to the console.
