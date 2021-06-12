1d3d1748b7d31692c9c544948dc308f196087b29
reetah589 <reetah589@gmail.com>
2021-06-12 17:05:12 -0500

    db anf schema files added with code

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-//You’ll need to use the MySQL2 package 
-
-//(Links to an external site.) 
+//You’ll need to use the MySQL2 package  
 
 //to connect to your MySQL database and perform queries, 
 
 //the Inquirer package 
-
-//(Links to an external site.) 
 
 //to interact with the user via the command-line, 
 
